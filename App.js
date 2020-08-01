dc1acc805f1a8414029fc2a7527d1253d6f42f5b
AttyC <antoniajcronin@gmail.com>
2020-08-01 18:09:18 +0100

    first working app

---
+++ App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  const [jokes, setJokes] = useState([]);
+
+  const loadJokes = async() => {
+
+    try {
+      const res = await fetch('https://official-joke-api.appspot.com/random_ten');
+      const jokes = await res.json();
+      setJokes(jokes);
+    } catch(err){
+      console.error(err); 
+    }
+
+  }
+
+  useEffect(() => {
+    loadJokes();
+  }, [])
+
+  console.log('jokes', jokes)
+  return (
+    <View style={styles.container}>
+       <Text>Welcome to the Jokester App!</Text>
+      <StatusBar style="auto" />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
